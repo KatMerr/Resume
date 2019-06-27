6b97e9e10acca4ea2995dcd479e075cc71ea39dc
Matty Ice <mattjknerr@gmail.com>
2019-06-27 02:06:59 -0400

    Init Commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 
 import { Provider } from 'react-redux'
-import Store from './redux/store'
+import Store from './redux/store/store'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
